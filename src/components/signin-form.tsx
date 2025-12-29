05b610cc0b28f1abd64204aa417a93d38d05afae
Mehedi Hasan <mehedi@etlimited.net>
2025-12-29 09:47:14 +0600

    some changes

--- src/components/signin-form.tsx
+++ src/components/signin-form.tsx
@@ -2,27 +2,26 @@
 import { useForm } from "@tanstack/react-form";
 import { toast } from "sonner";
 import z from "zod";
-import {
-  Field,
-  FieldError,
-  FieldGroup,
-  FieldLabel,
-  FieldSet,
-} from "@/components/ui/field";
+import { Field, FieldError, FieldGroup, FieldLabel, FieldSet } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
+import {
+  InputGroup,
+  InputGroupInput,
+  InputGroupAddon,
+  InputGroupButton,
+} from "@/components/ui/input-group";
+import { Button } from "./ui/button";
+import { useState } from "react";
+import { Eye, EyeOff } from "@hugeicons/core-free-icons";
+import { HugeiconsIcon } from "@hugeicons/react";
 
 const signInSchema = z.object({
-  username: z
-    .string()
-    .min(5, { error: "Username must be at least 5 characters long." })
-    .max(30, { error: "Username must be at most 30 characters long." }),
-  password: z
-    .string()
-    .min(8, { error: "Password must be at least 8 characters long." })
-    .max(128, { error: "Password must be at most 128 characters long." }),
+  username: z.string().min(1, { error: "Username is required." }),
+  password: z.string().min(1, { error: "Password is required." }),
 });
 
 export const SigninForm = () => {
+  const [showPassword, setShowPassword] = useState(false);
   const form = useForm({
     defaultValues: { username: "", password: "" },
     validators: { onSubmit: signInSchema },
@@ -39,29 +38,70 @@ export const SigninForm = () => {
     >
       <FieldSet>
         <FieldGroup>
-          <Field orientation="responsive" data-invalid>
-            <FieldLabel htmlFor="username" className="text-sm">
-              Username
-            </FieldLabel>
-            <FieldError>Choose a unique username for your account.</FieldError>
-            <Input
-              id="username"
-              type="text"
-              className="text-base! shadow-xs"
-              aria-invalid
-            />
-          </Field>
-          <Field orientation="responsive" data-invalid>
-            <FieldLabel htmlFor="password" className="text-sm">
-              Password
-            </FieldLabel>
-            <FieldError>Must be at least 8 characters long.</FieldError>
-            <Input
-              id="password"
-              type="password"
-              className="text-base! shadow-xs"
-              aria-invalid
-            />
+          <form.Field
+            name="username"
+            children={(field) => {
+              const isInvalid = field.state.meta.isTouched && !field.state.meta.isValid;
+              return (
+                <Field orientation="responsive" data-invalid={isInvalid}>
+                  <FieldLabel htmlFor={field.name} className="text-sm">
+                    Username
+                  </FieldLabel>
+                  <Input
+                    id={field.name}
+                    name={field.name}
+                    value={field.state.value}
+                    onBlur={field.handleBlur}
+                    onChange={(e) => field.handleChange(e.target.value)}
+                    aria-invalid={isInvalid}
+                    autoComplete="off"
+                    className="text-base! shadow-xs"
+                  />
+                  {isInvalid && <FieldError errors={field.state.meta.errors} />}
+                </Field>
+              );
+            }}
+          />
+          <form.Field
+            name="password"
+            children={(field) => {
+              const isInvalid = field.state.meta.isTouched && !field.state.meta.isValid;
+              return (
+                <Field orientation="responsive" data-invalid={isInvalid}>
+                  <FieldLabel htmlFor={field.name} className="text-sm">
+                    Password
+                  </FieldLabel>
+                  <InputGroup>
+                    <InputGroupInput
+                      id={field.name}
+                      type={showPassword ? "text" : "password"}
+                      name={field.name}
+                      value={field.state.value}
+                      onBlur={field.handleBlur}
+                      onChange={(e) => field.handleChange(e.target.value)}
+                      aria-invalid={isInvalid}
+                      autoComplete="off"
+                      className="text-base!"
+                    />
+                    <InputGroupAddon align="inline-end">
+                      <InputGroupButton
+                        size="icon-xs"
+                        aria-label={showPassword ? "Hide password" : "Show password"}
+                        onClick={() => setShowPassword(!showPassword)}
+                      >
+                        <HugeiconsIcon icon={showPassword ? EyeOff : Eye} />
+                      </InputGroupButton>
+                    </InputGroupAddon>
+                  </InputGroup>
+                  {isInvalid && <FieldError errors={field.state.meta.errors} />}
+                </Field>
+              );
+            }}
+          />
+          <Field orientation="responsive">
+            <Button type="submit" className="hover:bg-primary/80 text-sm">
+              Submit
+            </Button>
           </Field>
         </FieldGroup>
       </FieldSet>
