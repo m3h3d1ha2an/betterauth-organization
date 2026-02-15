630e4cc0c1c4176975e7eb6ec3248dad14005f80
Mehedi Hasan <mehedi@etlimited.net>
2026-02-15 11:48:07 +0600

    fixed: env load problem fixed with dotenvx

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
-import "./src/env";
 import type { NextConfig } from "next";
+import "./src/env";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   reactCompiler: true,
   typedRoutes: true,
   images: { remotePatterns: [{ protocol: "https", hostname: "*" }] },
+  transpilePackages: ["@t3-oss/env-nextjs", "@t3-oss/env-core"],
 };
 
 export default nextConfig;
--- prisma.config.ts
+++ prisma.config.ts
@@ -1,10 +1,10 @@
-import "./src/env";
-import { defineConfig, env } from "prisma/config";
+import { defineConfig } from "prisma/config";
+import { env } from "./src/env";
 
 const prismaConfig = defineConfig({
-  schema: "./prisma",
-  migrations: { path: "./prisma/migrations" },
-  datasource: { url: env("DATABASE_URL") },
+  schema: "prisma",
+  migrations: { path: "prisma/migrations" },
+  datasource: { url: env.DATABASE_URL },
 });
 
 export default prismaConfig;
